9d50a80afa8f6bac4db543fd54baad86e4838b17
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-10-23 20:47:14 +0200

    refactor integration files, move styles to a separate file

---
+++ src/alg.cubing.net/vscubing/integration.js
@@ -0,0 +1,32 @@
+angular.element(document).ready(() => {
+  const api = (() => {
+    const controllerElement = document.querySelector(
+      '[data-ng-controller="algxController"]',
+    );
+    const $scope = angular.element(controllerElement).scope();
+
+    $scope.$watch("alg", () =>
+      document.querySelector("#algorithm").setSelectionRange(0, 0),
+    );
+
+    function importReconstruction(scramble, reconstruction) {
+      const props = {
+        puzzle: $scope.puzzle_map["3x3x3"],
+        type: $scope.type_map["reconstruction"],
+        title: "Solve by Bohdan Chornokondratenko",
+        setup: scramble,
+        alg: reconstruction,
+      };
+      $scope.$apply(() => {
+        $scope.reset();
+        for (i in props) {
+          $scope[i] = props[i];
+        }
+      });
+    }
+
+    return { importReconstruction };
+  })();
+
+  window.api = api;
+});
